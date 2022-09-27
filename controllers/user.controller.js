283dc515c9e65abba766d346dd7c12311aa44d84
shama <assiddik001@gmail.com>
2022-09-27 14:05:15 +0600

    random user api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,32 @@
+const fs = require("fs");
+const data = fs.readFileSync("user.json", "utf8");
+
 module.exports.getAllUsers = (req, res) => {
   res.status(200).json({
     success: true,
     message: "success",
-    data: "All user found",
+    data: JSON.parse(data),
   });
+};
+module.exports.getRandomUser = (req, res) => {
+  const parsedData = JSON.parse(data);
+  const length = parsedData.length - 1;
+  const randomNumber = () => {
+    const number = Math.round(Math.random() * 100);
+    if (number > length) {
+      randomNumber();
+    } else {
+      const randomId = number;
+      let randomUser = [];
+      const randomData = parsedData[randomId];
+      randomUser.push(randomData);
+      res.status(200).json({
+        success: true,
+        message: "success",
+        // data: JSON.parse(data),
+        data: randomUser,
+      });
+    }
+  };
+  randomNumber();
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,7 @@ const userControllers = require("../controllers/user.controller");
 const router = express.Router();
 
 router
-  .route("/")
+  .route("/all")
   /**
    * @api {get} /user All user
    * @apiDescription Get all the user
@@ -19,7 +19,25 @@ router
    * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
    * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
    */
-  .get(userControllers.getAllUsers)
-  .post();
+  .get(userControllers.getAllUsers);
+
+router
+  .route("/random")
+  /**
+   * @api {get} /user All user
+   * @apiDescription Get all the user
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the user.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .get(userControllers.getRandomUser);
 
 module.exports = router;
